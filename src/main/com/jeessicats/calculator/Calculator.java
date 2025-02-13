55abd056cc12f3d3f2fadaa4331eb6bdf0283527
jeessicats <jeessicats@gmail.com>
2025-02-13 19:22:43 -0300

    More test examples

--- src/main/com/jeessicats/calculator/Calculator.java
+++ src/main/com/jeessicats/calculator/Calculator.java
@@ -1,7 +1,22 @@
 package com.jeessicats.calculator;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class Calculator {
     public int add(int number1, int number2) {
         return number1 + number2;
+    }
+
+    public String calculateAnnuityPayment(String R, int t, String r, int n) {
+        BigDecimal rn = new BigDecimal(r).divide(new BigDecimal(n));
+        BigDecimal rnPlusOne = BigDecimal.ONE.add(rn);
+        BigDecimal rnPlusOnePownt = rnPlusOne.pow(n * t);
+        BigDecimal rnPlusOnePowntMinusOne = rnPlusOnePownt.subtract(BigDecimal.ONE);
+        BigDecimal rnPlusOnePowntMinusOnern = rnPlusOnePowntMinusOne.divide(rn);
+        BigDecimal RrnPlusOnePowntMinusOnern = rnPlusOnePowntMinusOnern.multiply(new BigDecimal(R));
+        NumberFormat currencyFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        return currencyFormat.format(RrnPlusOnePowntMinusOnern);
     }
 }
--- src/test/com/jeessicats/calculator/CalculatorTest.java
+++ src/test/com/jeessicats/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package com.jeessicats.calculator;
 
-import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +35,26 @@ public class CalculatorTest {
     public void testAddOnePlusMinusOne() {
         int sum = calculator.add(1, -1);
         assertEquals(0, sum);
+    }
+
+    @Test
+    @Disabled
+    // @Ignore is the JUnit 4 equivalent of @Disabled
+    public void canAddMaxIntPlusOne() {
+        int sum = calculator.add(Integer.MAX_VALUE, 1);
+        assertEquals(Integer.MAX_VALUE + 1L, sum);
+    }
+
+    @Test
+    public void annuityPayment() {
+        String answer = calculator.calculateAnnuityPayment("22000", 7, ".06", 1);
+        assertEquals("$184,664.43", answer);
+    }
+
+    @Test
+    public void annuityPractice2() {
+        String answer = calculator.calculateAnnuityPayment("1200", 10, ".08", 4);
+        assertEquals("$72,482.38", answer);
     }
 
 }
